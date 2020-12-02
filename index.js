f92425ef18a2badde572cf622ba2d10e679a2542
angela <angela.liu@axcelerate.com.au>
2020-12-02 21:52:20 +1000

    8pass by myself

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ exports.stripPrivateProperties = (properties, arr) => {
   arr.forEach((obj) => {
     properties.forEach((prop) => {
       delete obj[prop];
-    }
-    )
+    })
   });
   return arr;
 };
@@ -22,31 +21,44 @@ exports.excludeByProperty = (property, arr) => {
 };
 exports.sumDeep = (arr) => {
   const newArr = arr.map((ele) => {
-    const arrOfVal = Object.values(ele);
-    let sum = 0;
-    for (i = 0; i < arrOfVal[0].length; i++) {
-      sum = sum + arrOfVal[0][i].val;
-    };
-    return { objects: sum };
+    // const arrOfVal = Object.values(ele);
+    // let sum = 0;
+    // for (i = 0; i < arrOfVal[0].length; i++) {
+    //   sum = sum + arrOfVal[0][i].val;
+    // };
+    // return { objects: sum };
+    const key = Object.keys(ele)[0];
+    const newObject = {};
+    const reducer = (acc, cur) => acc + cur.val;
+    const sum = ele.objects.reduce(reducer, 0);
+    newObject[key] = sum;
+    return newObject;
   });
   return newArr;
 };
-exports.applyStatusColor = (color, arrOfCode) => {
+exports.applyStatusColor = (colors, arrOfCode) => {
   let newArr = [];
   arrOfCode.forEach(
     (ele) => {
-      const valuesOfColor = Object.values(color);
-      const keysOfColor = Object.keys(color);
-      for (i = 0; i < valuesOfColor.length; i++) {
-        if (valuesOfColor[i].indexOf(ele.status) > -1) {
-          const codeWithColor = {
-            status: ele.status,
-            color: keysOfColor[i]
-          };
-          newArr.push(codeWithColor);
-          break;
+      const colorArr = Object.keys(colors);
+      colorArr.forEach((color) => {
+        if (colors[color].indexOf(ele.status) > -1) {
+          ele.color = color;
+          newArr.push(ele);
         }
-      }
+      })
+      // const valuesOfColor = Object.values(color);
+      // const keysOfColor = Object.keys(color);
+      // for (i = 0; i < valuesOfColor.length; i++) {
+      //   if (valuesOfColor[i].indexOf(ele.status) > -1) {
+      //     const codeWithColor = {
+      //       status: ele.status,
+      //       color: keysOfColor[i]
+      //     };
+      //     newArr.push(codeWithColor);
+      //     break;
+      //   }
+      // }
     }
   );
   return newArr;
@@ -55,29 +67,34 @@ exports.createGreeting = (cb, greetString) => {
   return name => cb(greetString, name);
 };
 exports.setDefaults = (objWithDefaultProp) => {
-  return obj => {
-    const defaultProps = Object.keys(objWithDefaultProp);
-    const defaultValues = Object.values(objWithDefaultProp);
-    for (i = 0; i < defaultProps.length; i++) {
-      if (Object.keys(obj).indexOf(defaultProps[i]) === -1) {
-        obj[defaultProps[i]] = defaultValues[i];
-      }
-    }
-    return obj;
-  };
+  return obj => Object.assign({ ...objWithDefaultProp }, obj);
+  // return obj => {
+  //   const defaultProps = Object.keys(objWithDefaultProp);
+  //   const defaultValues = Object.values(objWithDefaultProp);
+  //   for (i = 0; i < defaultProps.length; i++) {
+  //     if (Object.keys(obj).indexOf(defaultProps[i]) === -1) {
+  //       obj[defaultProps[i]] = defaultValues[i];
+  //     }
+  //   }
+  //   return obj;
+  // };
 
 };
-exports.fetchUserByNameAndUsersCompany = (searchValue, services) => new Promise(
+exports.fetchUserByNameAndUsersCompany = (userName, services) => new Promise(
   resolve => {
-    const searchResult = {};
-    Promise.all([services.fetchUsers(), services.fetchStatus()]).then(value => {
-      searchResult.status = value[1];
-      userIndex = value[0].findIndex(user => user.name === searchValue);
-      searchResult.user = value[0][userIndex];
-      return services.fetchCompanyById(searchResult.user.companyId)
-    }).then(res => {
-      searchResult.company = res;
-      resolve(searchResult);
-    });
-  }
-) 
+    const result = {};
+    Promise.all([
+      services.fetchUsers()
+        .then(res => {
+          const user = res.find(ele => ele.name === userName);
+          result.user = user;
+          return services.fetchCompanyById(user.companyId);
+        })
+        .then(res => result.company = res),
+      services.fetchStatus()])
+      .then((res) => {
+        result.status = res[1];
+        resolve(result);
+      });
+  });
+
